835ce9f4470fcb905fc1133b06fc572313ecf4d6
acer <acer@DESKTOP-9OJQ4LU>
2018-03-21 13:58:33 +0800

    添加项目文件。

---
+++ Company.IDAL/IBaseDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Model;
+
+namespace Company.IDAL
+{
+  public partial  interface IBaseDAL<T> where T:class,new()
+    {
+        /// <summary>
+        /// 添加
+        /// </summary>
+        /// <param name="t"></param>
+        void Add(T t);
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="t"></param>
+        void Delete(T t);
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="t"></param>
+        void Update(T t);
+        IQueryable<T> GetModels(Expressino<Func<T, bool>> whereLambda);
+    }
+}
---
+++ Company.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Company.UI.Startup))]
+namespace Company.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
